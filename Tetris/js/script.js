eff9b02e550f8034fd0fd86e97528c68b34b8e43
MSay2 <yoannmeclot@hotmail.com>
2019-04-12 14:18:34 +0200

    Update Project

--- Tetris/js/script.js
+++ Tetris/js/script.js
@@ -6,6 +6,9 @@
 
 var canvas = document.querySelector(".canvas");
 
+/**
+ * Define an height and width of @Canvas
+ */
 canvas.width = 400;
 canvas.height = 520;
 
@@ -19,6 +22,7 @@ var container = document.querySelector(".container-canvas");
 var rows = 26;
 var columns = 20;
 var square = 20;
+
 var BLACK = "#424242";
 var WHITE = "rgba(255, 255, 255, 0.1)"
 
@@ -37,8 +41,6 @@ var b = 0;
 var x = 0;
 var y = 0;
 
-var gameOver = false;
-
 /**
  * Get the all Form [Z, S, S, T, O, L, I, J]
  * and join the colors of each elements
@@ -49,38 +51,146 @@ var PIECES =
   [S, "#2C9010"],
   [T, "#EB7C2B"],
   [O, "#4D5AA8"],
-  [L, "#633184"],
+  [L, "#6A1B9A"],
   [I, "#9FB501"],
   [J, "#F15D22"]
 ];
 
+/**
+ * Get @DIV tag for yhe Score, the Hours, the Game  Over message
+ */
 var score = document.querySelector(".score");
+var hours = document.querySelector(".hours-computer");
+var gameOverMessage = document.querySelector(".gameover-message");
 
+/**
+ * This is for the @SVG (Dragon Ball)
+ */
+var dragonball = document.querySelector(".dragon-ball");
+
+
+/**
+ * Create variable and instanciate the @Date class
+ */
+var date = new Date();
+
+/**
+ * Write a default value for the hours, the minutes and the seconds of this days
+ */
+hours.innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+/**
+ * Initiate a time manager to run every 1 second 
+ */
+setInterval(showHours, 1000);
+
+/**
+ * Get the hours, minutes and seconds of the days
+ */
+function showHours()
+{
+  var date = new Date();
+
+  hours.innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
+/**
+ * Create a default value for the point of the user
+ */
 var points = 0;
 
+/**
+ * Create a default variable to generate variables dynamically
+ */
+var number = 0;
+
+/**
+ * I use @querySelectorAll for get the all class with the name @score-saved
+ * 
+ * @querySelectorAll get the all elements and create a list 
+ * 
+ */
+var scoreSaved = document.querySelectorAll(".score-saved");
+
+/**
+ * Create a default variable for not refresh the number than is assigned
+ */
+var length;
+
+/**
+ * Create a variable for get the fist cookie saved
+ */
+var cook = getCookie("variableGenerated" + 1);
+
+/**
+ * See if the first cookie exist
+ * 
+ * If he exist, insert all points saved in tag @p
+ */
+if (cook)
+{
+  scoreSaved[0].innerHTML = getCookie("variableGenerated" + getCookie("size"));
+  scoreSaved[1].innerHTML = getCookie("variableGenerated" + (getCookie("size") -1));
+  scoreSaved[2].innerHTML = getCookie("variableGenerated" + (getCookie("size") -2));
+  scoreSaved[3].innerHTML = getCookie("variableGenerated" + (getCookie("size") -3));
+}
+else
+{
+  console.log("The variable cook is null");
+}
+
+/**
+ * Create a variable for initiate a dificulty
+ */
+
+var dificulty;
+
+/**
+ * Get the start button in HTML
+ */
 var buttonStart = document.querySelector(".button-start");
 
+/**
+ * Add a listener for listen the click of the user
+ * And execute the start function
+ */
 buttonStart.addEventListener("click", start);
 
 function start()
-{
+{  
+  /**
+   * Add of this condition for returned this function is nothing
+   * This is for stoped this function and not executing this function
+   */
   if (getState() == CHANGED)
   {
     return;
   }
 
+  /**
+   * Set my variable @state and modifying it
+   */
   setState(CHANGED);
 
-  if (gameOver == false)
+  /**
+   * After modifying my variable @state use another condition
+   * this will allow me to put my elements at an initial value
+   */
+  if (getState() == CHANGED)
   {
+    buttonStart.innerHTML = "";
     buttonStart.innerHTML = "Commencer";
+
+    gameOverMessage.style.opacity = "0";
+    gameOverMessage.style.visibility = "hidden";
+
+    dragonball.style.opacity = "0";
+    dragonball.style.visibility = "hidden";
   }
 
-  canvas = document.querySelector(".canvas");
-
-  canvas.width = 400;
-  canvas.height = 520;
-
+  /**
+   * @brush is returned on the drawing contexte of Canvas
+   */
   brush = canvas.getContext("2d");
 
   /**
@@ -271,6 +381,8 @@ function start()
     }
   }
 
+  // Create a function for blocked an form
+
   Piece.prototype.lock = function()
   {
     for (i = 0; i < this.formActive.length; i++)
@@ -292,6 +404,7 @@ function start()
       }
     }
 
+    // This is for a full line
     for(i = 0; i < rows; i++)
     {
       var isRow = true;
@@ -302,7 +415,7 @@ function start()
 
       if(isRow)
       {
-      // we move down all the rows above it
+      // We move down all the rows above it
         for(z = i; z > 1; z--)
         {
           for(b = 0; b < columns; b++)
@@ -310,14 +423,37 @@ function start()
             board[z][b] = board[z -1][b];
           }
         }
-        // the top row board[0][..] has no row above it
+        // The top row board[0][..] has no row above it
         for(n = 0; n < columns; n++)
         {
           board[0][n] = BLACK;
         }
 
-        // icrement the points for the score
+        // Increment the points for the score
         points += 5;
+
+        // +1 each time for generate a number (1 then 2 then 3 then 4...etc)
+        number++;
+
+        /**
+         * Create variable for cookie and appends a viarable @number
+         * each time this is generate a new name by JavaScript
+         * Example : @variableGenerated1 then @variableGenerated2 then @variableGenerated3 then @variableGenerated4 etc...
+         */
+        var variable = "variableGenerated" + number; // this is here
+      
+        /**
+         * Then get cookie function and write a new point (score of user)
+         * With the new name created by JavaScript (Create a name dynamically)
+         */
+        setCookie(variable, points, 300);
+
+        /**
+         * Then, set a size (number) for the number of cookie saved
+         * 
+         * Create a size()
+         */
+        setCookie("size", number, 300);
       }
     }
     drawBoard();
@@ -334,7 +470,7 @@ function start()
     {
       for(e = 0; e < piece.length; e++)
       {
-      // if the square is empty, we skip it
+      // if the square is empty
         if(!piece[i][e])
         {
           continue;
@@ -376,40 +512,60 @@ function start()
   {
       if (!gameOver)
       {
-        if(event.keyCode == 37)
+        if(event.keyCode == 37) // Arrow Left 
         {
           random.moveLeft();
           //dropStart = Date.now();
         }
-        else if(event.keyCode == 38)
+        else if(event.keyCode == 38) // Arrow Top
         {
           random.rotation();
           //dropStart = Date.now();
         }
-        else if(event.keyCode == 39)
+        else if(event.keyCode == 39) // Arrow Right
         {
           random.moveRight();
           //dropStart = Date.now();
         }
-        else if(event.keyCode == 40)
+        else if(event.keyCode == 40) // Arrow Bottom
         {
           random.moveDown();
         }
       }
   }
 
-// drop the piece every 1sec
+// Moving the piece every 1 second
   var dropStart = Date.now();
+  var gameOver = false;
   function drop()
   {
     var now = Date.now();
     var milisecondes = now - dropStart;
-
-    if(milisecondes > 1000)
+    if (points > 50)
     {
-        random.moveDown();
-        dropStart = Date.now();
+      dificulty = 2;
     }
+    else if (points < 50)
+    {
+      dificulty = 1;
+    }
+    if (dificulty == 1)
+    {
+      if(milisecondes > 1000)
+      {
+          random.moveDown();
+          dropStart = Date.now();
+      }
+    }
+    else if (dificulty == 2)
+    {
+      if(milisecondes > 100)
+      {
+          random.moveDown();
+          dropStart = Date.now();
+      }
+    }
+
     if (!gameOver)
     {
       requestAnimationFrame(drop);
@@ -419,29 +575,110 @@ function start()
       buttonStart.innerHTML = "Recommencer";
       setState(UNCHANGED);
 
-      
+      gameOverMessage.style.visibility = "visible";
+      gameOverMessage.style.opacity = "1";
+
+      dragonball.style.visibility = "visible";
+      dragonball.style.opacity = "1";
+
       /**
-       * var date = new Date();
-       * 
-       * element.innerHTML = date.toLocaleTimeString();
+       * Refresh a new size (number) 
        */
+      length = getCookie("size");
+      setCookie("size", length, 300);
+
+      /**
+       * Insert the values in tag @p for see the scores of user
+       */
+      scoreSaved[0].innerHTML = getCookie("variableGenerated" + getCookie("size"));
+      scoreSaved[1].innerHTML = getCookie("variableGenerated" + (getCookie("size") -1));
+      scoreSaved[2].innerHTML = getCookie("variableGenerated" + (getCookie("size") -2));
+      scoreSaved[3].innerHTML = getCookie("variableGenerated" + (getCookie("size") -3));
     }
   }
 
   drop();
 }
 
-var state = 0;
-
+/**
+ * Create a variable for create a different state
+ */
 var CHANGED = 1;
 var UNCHANGED = 2;
 
+/**
+ * Assign a default state to my variable @state
+ */
+var state = UNCHANGED;
+
+/**
+ * This cookie
+ */
+var pointsOfScore = "points_of_score";
+
+/**
+ * Create a setter for the variable @state
+ * 
+ * @param {Integer} state 
+ */
 function setState(state)
 {
   this.state = state;
 }
 
+/**
+ * Create a getter for the variable @state
+ */
 function getState()
 {
   return state;
 }
+
+function setCookie(cookieName, cookieValue, expiresDays) 
+{
+  var date = new Date();
+  date.setTime(date.getTime() + (expiresDays * 24 * 60 * 60 * 1000));
+
+  var expires = "expires=" + date.toGMTString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+function getCookie(cookieName) 
+{
+  var name = cookieName + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var list = decodedCookie.split(';');
+
+  for(var i = 0; i < list.length; i++) 
+  {
+    var cookie = list[i];
+    while (cookie.charAt(0) == ' ') 
+    {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(name) == 0) 
+    {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+  return "";
+}
+
+
+// function checkCookie() 
+// {
+//   var userPoints = getCookie(pointsOfScore);
+
+//   if (userPoints != "") 
+//   {
+//     alert("Welcome again " + userPoints);
+//   } 
+//   else
+//   {
+//     userPoints = prompt("Please enter your name:", "");
+//     if (userPoints != "" && userPoints != null) 
+//     {
+//       setCookie(pointsOfScore, userPoints, 30);
+//     }
+//   }
+// }
